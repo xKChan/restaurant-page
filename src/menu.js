bee1caa9ef82a55ee6e8f251c5f8d8a579942a2d
KC <xkylechan@gmail.com>
2023-08-24 03:05:31 -0600

    Added all menu items to menu section of webpage

--- src/menu.js
+++ src/menu.js
@@ -8,6 +8,9 @@ function createMenu() {
 
   menu.appendChild(menuTitle);
 
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("menuContainer");
+
   const appitizers = document.createElement("div");
   appitizers.classList.add("appitizers");
 
@@ -29,8 +32,105 @@ function createMenu() {
       "$14.99"
     )
   );
+  appitizers.appendChild(
+    createMenuItem(
+      "Potato Skins",
+      "Crispy, golden potato skins fried in duck fat topped with bacon, green onions, and our special house blend of cheese.  Served with sour cream and salsa on the side",
+      "$8.99"
+    )
+  );
+  appitizers.appendChild(
+    createMenuItem(
+      "Death Poppers",
+      "Little poppers that will melt your mouth.  These poppers are stuffed with mozzarealla and Carolina Reapers.",
+      "$14.99"
+    )
+  );
+  appitizers.appendChild(
+    createMenuItem(
+      "Chicken Wings",
+      "10 Flavours to pick from: Honey Garlic, BBQ, Greek, Salt & Pepper, Lemon Pepper, Spicy, Blazin hot, Honey Hot, Smokey, and Original.  Served with a side of ranch and carrots",
+      "$14.99"
+    )
+  );
 
-  menu.appendChild(appitizers);
+  const bbq = document.createElement("div");
+  bbq.classList.add("bbq");
+
+  const bbqTitle = document.createElement("h3");
+  bbqTitle.textContent = "BBQ";
+
+  bbq.appendChild(bbqTitle);
+  bbq.appendChild(
+    createMenuItem(
+      "Rack of Ribs",
+      "Rack of our famous ribs that have been smoked for 5 hours+.  Served with a side of coleslaw and french fries",
+      "$39.99"
+    )
+  );
+  bbq.appendChild(
+    createMenuItem(
+      "Ribeye",
+      "Our bone in Ribeye is grilled to perfection, we recommend having it Medium Rare, but can be cooked however you want it.  Served with a baked potato and mixed vegetables",
+      "$69.99"
+    )
+  );
+  bbq.appendChild(
+    createMenuItem(
+      "Free Range Chicken",
+      "Our free range chicken are smoked daily for 4+ hours to embrace all the smokey flavours.  Served with french fries and potato salad",
+      "$59.99"
+    )
+  );
+  bbq.appendChild(
+    createMenuItem(
+      "Smoked Brisket",
+      "*Customer Favorite* Our famous smoked brisket that has been smoked for 18+ hours.  Served with coleslaw and french fries",
+      "$59.99"
+    )
+  );
+  bbq.appendChild(
+    createMenuItem(
+      "Pulled Pork",
+      "Tender pulled pork that just melts in your mouth.  Served with coleslaw and potato salad",
+      "$39.99"
+    )
+  );
+
+  const desserts = document.createElement("div");
+  desserts.classList.add("desserts");
+
+  const dessertsTitle = document.createElement("h3");
+  dessertsTitle.textContent = "Desserts";
+
+  desserts.appendChild(dessertsTitle);
+  desserts.appendChild(
+    createMenuItem(
+      "Apple Pie",
+      "A Large slice of homemade Apple Pie served with a scoop of vanilla ice cream on top.",
+      "$12.99"
+    )
+  );
+  desserts.appendChild(
+    createMenuItem(
+      "Chocolate Cake",
+      "A classic slice of chocolate cake with brownie crumbles on top",
+      "$9.99"
+    )
+  );
+  desserts.appendChild(
+    createMenuItem(
+      "Crème Brûlée",
+      "An exquisitely rich and elegant dessert",
+      "$11.99"
+    )
+  );
+
+  menuContainer.appendChild(appitizers);
+  menuContainer.appendChild(bbq);
+  menuContainer.appendChild(desserts);
+
+  menu.appendChild(menuContainer);
 
   return menu;
 }
