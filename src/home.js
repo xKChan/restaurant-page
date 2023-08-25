568bc8e0e2a7328df0ef7a294a55d886ad54214d
KC <xkylechan@gmail.com>
2023-08-25 00:01:06 -0600

    Style and created Home page of restaurnt

--- src/home.js
+++ src/home.js
@@ -2,6 +2,32 @@ function createHome() {
   const home = document.createElement("div");
   home.classList.add("home");
 
+  const homeContainer = document.createElement("div");
+  homeContainer.classList.add("homeContainer");
+
+  const smokeHouse = document.createElement("div");
+  smokeHouse.classList.add("smokeHouse");
+
+  // const img = document.createElement("img");
+  // img.src = "../dist/imgs/meal.jpg";
+
+  const welcome = document.createElement("h1");
+  welcome.classList.add("welcome");
+  welcome.textContent = "WELCOME TO YYC SMOKEHOUSE";
+
+  const homePara = document.createElement("p");
+  homePara.classList.add("homePara");
+  homePara.textContent =
+    "Step into a world of smoky delights at YYC Smokehouse! Our passion lies in crafting mouthwatering dishes infused with the irresistible flavors of the smokehouse. From tender ribs to savory brisket, every bite is a journey into culinary excellence. Join us and experience the art of smoky goodness!";
+
+  smokeHouse.appendChild(welcome);
+  smokeHouse.appendChild(homePara);
+
+  homeContainer.appendChild(smokeHouse);
+  // homeContainer.appendChild(img);
+
+  home.appendChild(homeContainer);
+
   return home;
 }
 
